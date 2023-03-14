ba475be7c9963f48a3eca69e96532b99be7cccf3
Emin0101 <74253442+Emin0101@users.noreply.github.com>
2023-03-14 16:35:45 +0100

    Update and rename SQLQuery2.sql to SQLQuery.sql

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,167 +1,21 @@
 
+
+
+
+
+-- we begin by checking each table for errors to be cleaned
+-- we start from the employees table
 SELECT *
 FROM dbo.employees
 
 
-SELECT *
-FROM dbo.edetails
 
-
-SELECT *
-FROM dbo.supervisors
-
-SELECT *
-FROM dbo.qualityperformance
-
-
-
---creare una tabella con i worker_ID dei lavoratori, anno di assunzione, professione, rating da 1 a 5
---il supervisor_ID che l'ha valutato
---rating nullo per i lavoratori assunti nel 2022
-
-
-
-
-CREATE TABLE qualityperformance (
-
-    worker_ID INT PRIMARY KEY NOT NULL IDENTITY(1,1),
-	hiringyear NVARCHAR(50) NOT NULL,
-	wq_performance INT,
-	profession NVARCHAR(100),
-	supervisor_ID INT 
-
-)
-
-SELECT *
-FROM dbo.qualityperformance
-
-
-
-
-select profession, count(profession) as Nprofession
-from dbo.edetails
-group by profession
-order by Nprofession desc
-
-
-
-
-INSERT INTO dbo.qualityperformance
-VALUES ('2018', 4, 'Painter', 2),
-       ('2019', 4, 'Carpenter', 2),
-       ('2021', 4, 'Electrician', 7),
-       ('2018', 3, 'Carpenter', 1),
-       ('2020', 4, 'Plumber', 9),
-       ('2019', 3, 'Construction Worker', 5),
-       ('2018', 4, 'Plumber', 9),
-       ('2018', 3, 'Construction Worker', 6),
-       ('2018', NULL, 'Project Manager', NULL),
-       ('2021', 4, 'Painter', 8),
-       ('2019', 4, 'Electrician', 7),
-       ('2021', 3, 'Painter', 8),
-       ('2019', 3, 'Construction Worker', 12),
-       ('2018', 2, 'Carpenter', 1),
-       ('2018', 3, 'Painter', 8),
-       ('2018', 5, 'Construction Worker', 6),
-       ('2022', NULL, 'Project Manager', 2),
-       ('2020', 4, 'Architect', 10),
-       ('2022', NULL, NULL, 1),
-       ('2020', 4, 'Plumber', 9),
-       ('2018', 4, 'Engineer', 2),
-       ('2018', 4, 'Painter', 8),
-       ('2018', 3, 'Painter', 8),
-       ('2019', 4, 'Safety Inspector', 2),
-       ('2022', NULL, 'Carpenter', 1),
-       ('2020', 4, 'Carpenter', 1),
-       ('2020', 3, 'Construction Worker', 5),
-       ('2018', 3, 'Electrician', 2),
-       ('2019', 4, 'Carpenter', 1),
-       ('2022', NULL, 'Electrician', 7),
-       ('2019', 4, 'Safety Inspector', 3),
-       ('2019', 2, 'Construction Worker', 12),
-       ('2019', 2, 'Painter', 8),
-       ('2022', NULL, 'Construction Worker', 5),
-       ('2018', 4, NULL, 2),
-       ('2022', NULL, 'Construction Worker', 6),
-       ('2018', 4, 'Construction Worker', 6),
-       ('2018', 4, 'Engineer', 4),
-       ('2018', 4, 'Construction Worker', 11),
-       ('2019', 3, 'Construction Worker', 6),
-       ('2019', 3, 'Carpenter', 1),
-       ('2018', 4, 'Carpenter', 1),
-       ('2021', 4, 'Construction Worker', 11),
-       ('2021', 3, 'Construction Worker', 6),
-       ('2022', NULL, 'Construction Worker', 6),
-       ('2019', 4, 'Carpenter', 1),
-       ('2018', NULL, 'Secretary', NULL),
-       ('2020', 4, 'Construction Worker', 12),
-       ('2021', 4, 'Construction Worker', 11),
-       ('2022', NULL, 'Construction Worker', 11),
-       ('2018', 5, 'Construction Worker', 2),
-       ('2018', 5, 'Construction Worker', 2),
-       ('2018', 3, 'Construction Worker', 12),
-       ('2021', 5, 'Plumber', 9),
-       ('2018', 4, 'Architect', 10),
-       ('2018', 5, 'Construction Worker', 11),
-       ('2018', 2, 'Construction Worker', 5),
-       ('2019', 4, 'Construction Worker', 5),
-       ('2018', 3, 'Construction Worker', 2),
-       ('2022', NULL, 'Construction Worker', 12),
-       ('2018', 4, 'Construction Worker', 11),
-       ('2022', NULL, 'Construction Worker', 12),
-       ('2022', NULL, 'Architect', 10),
-       ('2018', 4, 'Construction Worker', 6),
-       ('2020', 2, NULL, 12),
-       ('2021', 4, 'Construction Worker', 11),
-       ('2018', 5, 'Plumber', 2),
-       ('2018', NULL, 'Secretary', NULL),
-       ('2019', 4, 'Construction Worker', 5),
-       ('2022', NULL, 'Painter', 8),
-       ('2019', 3, 'Construction Worker', 5),
-       ('2018', 5, 'Engineer', 4),
-       ('2018', 4, 'Construction Worker', 2),
-       ('2021', 5, 'Electrician', 7),
-       ('2020', 3, 'Electrician', 7),
-       ('2020', 5, 'Carpenter', 1)
-
-
-
-
-
-
-SELECT *
-FROM dbo.employees
-
-
-SELECT *
-FROM dbo.edetails
-
-
-SELECT *
-FROM dbo.supervisors
-
-SELECT *
-FROM dbo.qualityperformance
-
-
-
-
--- l'obbiettivo dell'impresa edile è quella di migliorare la qualità del lavoro e gli incassi
--- ciò verrà fatto analizzando il dataset dei lavoratori per vedere se ci sono cambiamenti da fare per il miglioramento degli incassi
--- si parte sistemando le tabelle
-
-
-SELECT *
-FROM dbo.employees
-
--- analizziamo i tipi di dati che abbiamo nelle tabelle
-
+-- we check the format of the data
 exec sp_help employees
 
 
 
--- cambiamo il tipo di dato da nvarchar a date per la colonna dateofbirth
-
+-- we convert the dateofbirth column from NVARCHAR to DATE format
 UPDATE dbo.employees
 SET dateofbirth = CONVERT(NVARCHAR(100), CONVERT(date, dateofbirth, 105))
 ALTER TABLE dbo.employees
@@ -170,13 +24,17 @@ ALTER COLUMN dateofbirth date
 
 
 
+
+
+-- we move to the edetails table
 SELECT *
 FROM dbo.edetails
 
 exec sp_help edetails
 
--- uniamo il giorno mese e anno in un'unica colonna
 
+-- we notice that we can join the hiringday, hiringmonth and hiringyear together to create a single column
+-- we convert the hiringyear column from int to nvarchar in order to cast the 3 columns together 
 UPDATE dbo.edetails
 SET hiringyear = CONVERT(INT, CONVERT(NVARCHAR(50), hiringyear))
 ALTER TABLE dbo.edetails
@@ -187,11 +45,10 @@ SET hiringyear = CONVERT(DATE, CAST(hiringyear AS VARCHAR(50)) + '-' +
                                CAST(hiringmonth AS VARCHAR(100)) + '-' +
                                CAST(hiringday AS VARCHAR(100))) 
 
-SELECT *
-FROM dbo.edetails
 
--- eliminiamo le colonne di hiringday e hiringmonth e cambiamo nome della colonna hiringyear
 
+-- we delete the hiringday and hiringmonth columns and we change the name of the hiringyear column to hiringdate since we have have combined the
+-- hiringday and hiringmonth columns to the hiringyear column
 ALTER TABLE dbo.edetails
 DROP COLUMN hiringday, hiringmonth
 
@@ -201,9 +58,12 @@ EXEC sp_RENAME 'edetails.hiringyear' , 'hiringdate', 'COLUMN'
 
 
 
--- nella tabella qualityperformance ci mancano dei valori che dovrebbero esserci quindi li aggiungiamo perché sono 
--- facilmente rintracciabili
 
+
+
+
+-- in the qualityperformance table there are missing values in the profession column but are easily traceable from the other tables
+-- we check which values are missing
 SELECT *
 FROM dbo.qualityperformance
 
@@ -213,13 +73,13 @@ WHERE profession IS NULL
 
 
 
--- 3 colonne sono vuote, rintracciamole grazie alle tabelle
-
+-- we have 3 missing values, we trace them from the edetails table using the worker_ID values
 SELECT worker_ID, profession
 FROM dbo.edetails
 WHERE worker_ID IN (19, 35, 65)
 
 
+-- after finding the data we add the missing values to the qualityperformance table
 UPDATE dbo.qualityperformance
 SET profession = 'Carpenter'
 WHERE worker_ID = 19
@@ -233,14 +93,19 @@ SET profession = 'Construction Worker'
 WHERE worker_ID = 65
 
 
--- controlliamo
--- ferfetto
 
 
 
--- ANALISI
--- controlliamo che tipo di lavoratori ci sono nel dataset in base a età e sesso
 
+
+
+
+-- AFTER CLEANING THE DATA WE BEGIN ANALYZING THE TABLES
+
+
+
+-- we are interested in checking the demographics of the workers
+-- we began by analyzing the ages of the workers
 SELECT *
 FROM dbo.employees
 
@@ -248,18 +113,8 @@ FROM dbo.employees
 SELECT AVG(age) AS averageAGE, MAX(age) AS maximumAGE, MIN(age) AS minimumAGE
 FROM dbo.employees
 
--- vediamo quante fasce di età ci sono e creiamo una nuova colonna
 
-SELECT COUNT(age) AS over40
-FROM dbo.employees
-WHERE age > 40
-
-SELECT *
-FROM dbo.employees
-WHERE age > 30 AND age < 40
-
-
-
+-- we check the different age ranges and we decide to create a new column dividing them in 4 ranges (young, adult, middle and older)
 SELECT *
 , CASE WHEN age > 50 THEN 'olderW'
        WHEN age < 50 AND age > 40 THEN 'middleW'
@@ -283,23 +138,23 @@ SET ageRange = CASE WHEN age >= 50 THEN 'olderW'
 SELECT ageRange, COUNT(ageRange) AS average
 FROM dbo.employees
 GROUP BY ageRange
+-- we notice that there are a lot of people over 40 years old
 
--- vediamo che ci sono tanti lavoratori over 40
 
 
--- controlliamo il sesso e in base all'età che tipi di professione ci sono
 
-SELECT gender, COUNT(gender)
+-- we check how many women work in the company in total
+SELECT gender, COUNT(gender) 
 FROM dbo.employees
-GROUP BY gender AS gendercount  --11 donne e 65 uomini
-
---vediamo le professioni in base a sesso dove i lavoratori sono over 50
+GROUP BY gender  --11 women in total
 
 
 
 
--- id 4 e 13 possibile licenziamento, vediamo per le altre fasce di età
 
+
+-- we decide to check wq_performance by age range, to do that we need to join the employees, edetails and qualityperformance tables
+-- we start from the 'olderW' range of age
 SELECT employees.worker_ID, firstname, lastname, gender, age, COUNT(ageRange) AS AgeCount, edetails.profession, 
 COUNT(edetails.profession) AS ProfessionCount, wq_performance, hourlypay, hiringyear
 FROM dbo.employees JOIN dbo.edetails
@@ -309,14 +164,13 @@ ON employees.worker_ID = qualityperformance.worker_ID
 WHERE ageRange = 'olderW' AND wq_performance < 4
 GROUP BY employees.worker_ID, firstname, lastname, gender, age, edetails.profession, wq_performance, hourlypay, hiringyear    
 ORDER BY wq_performance DESC
-
--- 7 da 3
-
+-- 7 workers have 3 wq_performance
 
 
 
 
 
+-- now we check the 'middleW' age range
 SELECT employees.worker_ID, firstname, lastname, gender, age, COUNT(ageRange) AS AgeCount, edetails.profession, 
 COUNT(edetails.profession) AS ProfessionCount, wq_performance, hourlypay, hiringyear
 FROM dbo.employees JOIN dbo.edetails
@@ -326,10 +180,12 @@ ON employees.worker_ID = qualityperformance.worker_ID
 WHERE ageRange = 'middleW' AND wq_performance < 4
 GROUP BY employees.worker_ID, firstname, lastname, gender, age, edetails.profession, wq_performance, hourlypay, hiringyear    
 ORDER BY wq_performance DESC
+-- 3 workers with 3 wq_performance and 2 workers with 2
 
 
--- 3 persone con 3, 2 con 2
 
+
+-- now we check the 'adultW' age range
 SELECT employees.worker_ID, firstname, lastname, gender, age, COUNT(ageRange) AS AgeCount, edetails.profession, 
 COUNT(edetails.profession) AS ProfessionCount, wq_performance, hourlypay, hiringyear
 FROM dbo.employees JOIN dbo.edetails
@@ -337,14 +193,14 @@ ON employees.worker_ID = edetails.worker_ID
 JOIN dbo.qualityperformance
 ON employees.worker_ID = qualityperformance.worker_ID
 WHERE ageRange = 'adultW' AND wq_performance < 4
-GROUP BY employees.worker_ID, firstname, lastname, gender, age, edetails.profession, wq_performance, hourlypay, hiringyear    -- 1 da 2, 1 da 3 (33, 53)
+GROUP BY employees.worker_ID, firstname, lastname, gender, age, edetails.profession, wq_performance, hourlypay, hiringyear    
 ORDER BY wq_performance DESC
-
---5 persone con 3, 3 con 2
-
+-- 5 people with 3 wq_performance and 3 workers with 2
 
 
 
+
+-- finally we check the 'youngW' age range
 SELECT employees.worker_ID, firstname, lastname, gender, age, COUNT(ageRange) AS AgeCount, edetails.profession, 
 COUNT(edetails.profession) AS ProfessionCount, wq_performance, hourlypay, hiringyear
 FROM dbo.employees JOIN dbo.edetails
@@ -354,13 +210,13 @@ ON employees.worker_ID = qualityperformance.worker_ID
 WHERE ageRange = 'youngW' AND wq_performance < 4
 GROUP BY employees.worker_ID, firstname, lastname, gender, age, edetails.profession, wq_performance, hourlypay, hiringyear    
 ORDER BY wq_performance DESC
--- 1 con 3
+-- 1 worker with 3 wq_performance
 
 
 
 
 
--- dopo aver analizzato vediamo quanti sono i due di performance
+-- now we check how many are there with 2 wq_performance
 SELECT employees.worker_ID, firstname, lastname, gender, age, edetails.profession, 
 COUNT(edetails.profession) AS ProfessionCount, wq_performance, hourlypay, hiringyear
 FROM dbo.employees JOIN dbo.edetails
@@ -369,12 +225,15 @@ JOIN dbo.qualityperformance
 ON employees.worker_ID = qualityperformance.worker_ID
 WHERE wq_performance < 3
 GROUP BY employees.worker_ID, firstname, lastname, gender, age, edetails.profession, wq_performance, hourlypay, hiringyear    
-ORDER BY wq_performance DESC   -- 5 persone (3 cw, 1 carp, 1 paint)
+ORDER BY wq_performance DESC   
+-- 5 workers (3 construction workers, 1 carpenter and 1 painter)
 
 
 
 
---analizziamo per professione quanti sono i -4 di performance e se c'è almeno 1 con esperienza e uno giovane per ogni professione
+
+
+--analizziamo per professione quanti sono i -4 di performance e se c'Ã¨ almeno 1 con esperienza e uno giovane per ogni professione
 
 SELECT profession, COUNT(profession)
 FROM dbo.edetails
